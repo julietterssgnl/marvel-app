f17dfce10c6f2a5906d53855c75e0c86af82b140
Juliette ROSSIGNOL <juliette.rossignol18@gmail.com>
2024-09-25 18:24:34 +0200

    Use static list for characters

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 import './App.css'
+import characters from './data/characters.json'
+
 function App() {
   return (
     <>
     <h1>Marvel Characters</h1>
       <ul id="characters">
-        <li>
-          Beast
-        </li>
-        <li>
-          Captain America
-        </li>
-        <li>
-          Deadpool
-        </li>
+         {characters.map((character) =>(
+          <li key ={character.id}>
+            {character.name}
+          </li>
+         ))}
       </ul>
     </>
   )
 }
+
 export default App

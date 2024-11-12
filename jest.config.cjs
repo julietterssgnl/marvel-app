13df3bee34f60d7c09257b0199e6b8dba990f26f
Juliette ROSSIGNOL <juliette.rossignol18@gmail.com>
2024-11-12 17:19:54 +0100

    ✅ Mise à jour de la configuration Jest et du fichier package-lock.json

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,15 +1,11 @@
 module.exports = {
-    testEnvironment: "jsdom"
-    ,
+    testEnvironment: "jsdom",
     transform: {
-    "^.+\\.jsx?$": "babel-jest"
-    ,
+      "^.+\\.jsx?$": "babel-jest",
     },
     collectCoverageFrom: [
-    "src/**/*.{js,jsx}"
-    , // Collect coverage from all js or jsx files in src folder
-    "!src/**/*.test.{js,jsx}"
-    , // Exclude test files from coverage
+      "src/**/*.{js,jsx}", // Collect coverage from all js or jsx files in src folder
+      "!src/**/*.test.{js,jsx}", // Exclude test files from coverage
     ],
-    };
-    
+    testResultsProcessor: 'jest-sonar-reporter',
+  };

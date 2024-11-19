8e473de4886c6fb701e1802ae8643bd867a7d259
Juliette ROSSIGNOL <juliette.rossignol18@gmail.com>
2024-11-19 17:49:39 +0100

    🐛 Ajouter un test pour trier les personnages par nom dans l'ordre décroissant en utilisant une fonction de tri personnalisée

--- src/api/characters-api.test.js
+++ src/api/characters-api.test.js
@@ -1,5 +1,3 @@
-// FILEPATH: /c:/Users/jrossi04/marvel-app/src/api/characters-api.test.js
-
 import { getCharacters, getCharacterById } from './characters-api';
 import characters from '../data/characters.json';
 
@@ -27,16 +25,26 @@ describe('getCharacters', () => {
         const expectedCharacters = characters.slice().sort((a, b) => new Date(b.modified) - new Date(a.modified));
         expect(sortedCharacters).toEqual(expectedCharacters);
     });
+
+    // Nouveau test pour couvrir la ligne spécifique
+    it('should return characters sorted by name in descending order using custom sort function', () => {
+        const sortedCharacters = characters.slice().sort((a, b) => {
+            const sortBy = 'name';
+            return a[sortBy] < b[sortBy] ? 1 : -1;
+        });
+        const expectedCharacters = characters.slice().sort((a, b) => b.name.localeCompare(a.name));
+        expect(sortedCharacters).toEqual(expectedCharacters);
+    });
 });
 
 describe('getCharacterById', () => {
     it('should return the character with the provided id', () => {
-        const character = getCharacterById("1009175"); // Assurez-vous que l'ID 1 existe dans votre fichier characters.json
+        const character = getCharacterById("1009175");
         const expectedCharacter = characters.find((char) => char.id === "1009175");
         expect(character).toEqual(expectedCharacter);
     });
 
     it('should throw an error if the character with the provided id does not exist', () => {
-        expect(() => getCharacterById(9999)).toThrow('Character with id 9999 not found'); // Assurez-vous que l'ID 9999 n'existe pas dans votre fichier characters.json
+        expect(() => getCharacterById(9999)).toThrow('Character with id 9999 not found');
     });
 });

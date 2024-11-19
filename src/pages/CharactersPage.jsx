fb4fbe1b2c94ca98161d7f9efb9731a8ba1e5d6e
Juliette ROSSIGNOL <juliette.rossignol18@gmail.com>
2024-11-19 16:45:25 +0100

    ✅ Mise à jour de CharactersPage et routes, fonctionnalité de tri au point :)

--- src/pages/CharactersPage.jsx
+++ src/pages/CharactersPage.jsx
@@ -1,21 +1,44 @@
-// Cette façon de faire fonctionne, mais elle n'est pas optimale. Elle mélange la récupération des données et l'affichage des composants.
-// react_router nous permet de faire mieux en utilisant des hooks pour récupérer les données avant d'afficher le composant et ainsi de séparer récupération des données et affichage.
-// Adapter le code pour utiliser react_router et les hooks comme dans l'exemple du guide, grâce aux concepts de loader et useLoaderData . On appelera directement la fonction getCharacters dans le loader (pas de fonction fetch ).
-import { NumberOfCharacters } from '../components/NumberOfCharacters'; // Chemin d'accès pour l'importation
+// FILEPATH: /c:/Users/jrossi04/marvel-app/src/pages/CharactersPage.jsx
 
-import { useLoaderData } from "react-router-dom";
-import React, { useEffect } from 'react';
+import { NumberOfCharacters } from '../components/NumberOfCharacters'; // Chemin d'accès pour l'importation
+import { useLoaderData, useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
 
 export default function CharactersPage() {
     const characters = useLoaderData(); // Assurez-vous que cette fonction soit importée correctement
+    const navigate = useNavigate();
+
+    const [sortBy, setSortBy] = useState('name');
+    const [order, setOrder] = useState('asc');
+
     // Utilisé pour changer le titre de la page
     useEffect(() => {
         document.title = "Marvel App"; // Modification du titre de la page
     }, []);
 
+    const handleSortChange = (event) => {
+        setSortBy(event.target.value);
+        navigate(`/?sort=${event.target.value}&order=${order}`);
+    };
+
+    const handleOrderChange = (event) => {
+        setOrder(event.target.value);
+        navigate(`/?sort=${sortBy}&order=${event.target.value}`);
+    };
+
     return (
         <div>
             <h2>Marvel Characters</h2>
+
+            {/* Ajout de listes déroulantes pour les tris par nom et date de modification ainsi que ascending et descending */}
+            <select value={sortBy} onChange={handleSortChange}>
+                <option value="name">Name</option>
+                <option value="modified">Modified</option>
+            </select>
+            <select value={order} onChange={handleOrderChange}>
+                <option value="asc">Ascending</option>
+                <option value="desc">Descending</option>
+            </select>
             <ul>
                 {characters.map(character => (
                     <li key={character.id}>
